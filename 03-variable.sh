4292d7ba32e2043beab5857df555604ec31666d5
tunji <tunji.ijaola28@gmail.com>
2023-09-19 01:25:18 -0400

    using a variable log_file=/tmp/expense.log

--- 03-variable.sh
+++ 03-variable.sh
@@ -7,3 +7,8 @@ name=devops
 
 #how to access a variable with the use of $ sign
 echo value of a = $name
+
+#e.g logfile=/tmp/expense.log
+# for me to reference this all i have to do is add the $sign to log_file anywhere i have /tmp/expense.log
+#and it will automatically change it for me
+
