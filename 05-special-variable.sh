323122d35beb2f8e5920b93ce91046dea9fadc2c
tunji <tunji.ijaola28@gmail.com>
2023-09-19 21:36:58 -0400

    special variables

---
+++ 05-special-variable.sh
@@ -0,0 +1,9 @@
+# special variables are 0-n,*,@,#,?
+# ? is for exit status
+
+echo script name - $0
+echo first argument - $1
+echo second argument - $2
+echo all arguments - $@
+echo all argumnets - $*
+echo no of arguments - $#
