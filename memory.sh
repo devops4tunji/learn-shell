75c988d966bd47c335e8302cedc9f658357a184b
tunji <tunji.ijaola28@gmail.com>
2023-09-28 00:00:35 -0400

    adding conditions - free percentage and used percentage

--- memory.sh
+++ memory.sh
@@ -3,8 +3,8 @@ free=$(free | grep Mem | awk '{print $4}')
 
 #so to know the available space
 
-free_percentage=$(echo "$free / $total * 100" | bc -l | awk -F . '{print $1}')
-echo $free_percentage
+#free_percentage=$(echo "$free / $total * 100" | bc -l | awk -F . '{print $1}')
+#echo $free_percentage
 
 #note bc in this statement is just a basic calculator in shell.
 #  the use of -l
