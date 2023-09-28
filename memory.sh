a7daff9b4d9e40772de22813781bbc63362610b4
tunji <tunji.ijaola28@gmail.com>
2023-09-28 00:11:35 -0400

    adding conditions - free percentage and used percentage

--- memory.sh
+++ memory.sh
@@ -18,7 +18,7 @@ free=$(free | grep Mem | awk '{print $4}')
 used_percentage=$(echo "100 - ($free / $total * 100)" | bc -l | awk -F . '{print $1}')
 echo $used_percentage
 
-If [ $used_percentage -lt 50 ]; then
+if [ $used_percentage -lt 50 ]; then
  echo Mem stat is GREEN
 elif [ $used_percentage -lt 70 ]; then
  echo  Mem stat is ORANGE
