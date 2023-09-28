db5f38b52d36d6abecfa9d9cf97dae4fbf81e9a3
tunji <tunji.ijaola28@gmail.com>
2023-09-27 23:22:15 -0400

    learning calculation and using subsitute

--- memory.sh
+++ memory.sh
@@ -4,5 +4,6 @@ free=$(free | grep Mem | awk '{print $4}')
 #so to know the available space
 
 free_percentage=$(echo "$free / $total * 100" | bc)
+echo $free_percentage
 
 #note bc in this statement is just a basic calculator in shell.
