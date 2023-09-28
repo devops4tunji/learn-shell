8b430b2b6a79f5126a256980e24d9144f73944b2
tunji <tunji.ijaola28@gmail.com>
2023-09-27 23:52:12 -0400

    learning calculation and using subsitute and the bC( basic calculator) -l adding scale=2; made us see the figures in 2 decimal places

--- memory.sh
+++ memory.sh
@@ -10,3 +10,15 @@ echo $free_percentage
 #  the use of -l
 #  the use of awk -f
 #  . '{print $1}'
+
+#0 -50 used -> good
+#51-70 – orange
+#>70 – red
+
+If [ $used_percentage -lt 50]; then
+ Echo Mem stat is green
+elif [$used_percentage -lt 70]; then
+Echo Mem stat is orange
+else
+Echo Mem stat is red
+fi
