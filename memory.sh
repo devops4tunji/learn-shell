55986709531a544fc6744ccc82aafb7451b699d0
tunji <tunji.ijaola28@gmail.com>
2023-09-27 23:37:39 -0400

    learning calculation and using subsitute and the bC( basic calculator) -l adding scale=2; made us see the figures in 2 decimal places

--- memory.sh
+++ memory.sh
@@ -3,7 +3,10 @@ free=$(free | grep Mem | awk '{print $4}')
 
 #so to know the available space
 
-free_percentage=$(echo "scale=2; $free / $total * 100" | bc -l)
+free_percentage=$(echo "scale=2; $free / $total * 100" | bc -l | awk -f . '{print $1}')
 echo $free_percentage
 
 #note bc in this statement is just a basic calculator in shell.
+#  the use of -l
+#  the use of awk -f
+#  . '{print $1}'
